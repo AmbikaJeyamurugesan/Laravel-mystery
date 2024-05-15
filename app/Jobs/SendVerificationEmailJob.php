99efbeaf1a7aaceb18605dda0f2f59e108923f00
Ambika <ambikajeyamurugesan@gmail.com>
2024-05-15 18:06:40 +0530

    updated app directory

--- app/Jobs/SendVerificationEmailJob.php
+++ app/Jobs/SendVerificationEmailJob.php
@@ -2,30 +2,42 @@
 
 namespace App\Jobs;
 
+use App\Mail\VerifyCodeMail;
+use App\Models\User;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
 
 class SendVerificationEmailJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
+    protected $user;
+
     /**
      * Create a new job instance.
+     *
+     * @param User $user
      */
-    public function __construct()
+    public function __construct(User $user)
     {
-        //
+        $this->user = $user;
     }
 
     /**
      * Execute the job.
+     *
+     * @return void
      */
-    public function handle(): void
+    public function handle()
     {
-        //
+        // Fetch the user's verification code from the database
+        $user = User::find($this->user->id);
+
+        // Send the verification email
+        Mail::to($user->email)->send(new VerifyCodeMail($user));
     }
 }
